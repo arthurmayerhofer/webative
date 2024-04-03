e405eca75a4443ad3427b0c41a74af804d7b77e3
arthurmayerhofer <mayerhofer.tec@gmail.com>
2024-04-02 22:55:23 -0300

    Adicionando workflow

--- Program.cs
+++ Program.cs
@@ -8,12 +8,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-// Add CORS configuration here
-app.UseCors(options =>
-{
-    options.AllowAnyOrigin()
-        .AllowAnyMethod()
-        .AllowAnyHeader();
-});
-
 await builder.Build().RunAsync();
